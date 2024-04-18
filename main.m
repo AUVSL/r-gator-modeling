2990bef60ee7d0e1f0501e6c0386ca74b0b2d0bb
sdekhterman <sdekhterman@gmail.com>
2024-04-17 21:55:14 -0500

    naming + blocks are as simple as I can get them (maybe add plot of pos)

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 clear all; clc;
-fis  = readfis("controller.fis")
+fis  = readfis("controller.fis");
 
 % parameters
 caf  = 2.787e-4; % cornering stiffness of front tire
@@ -26,30 +26,14 @@ A = [a11, a12; a21, a22];
 B = [b11; b21];
 C = [1, 0; 0, 1];
 D = [0; 0];
-
 % put the state space equation into tranfer function form (output = tf * input) 
 [b,a]   = ss2tf(A,B,C,D); 
-v_lat   = tf(b(1,:), a)
+v_lat   = tf(b(1,:), a);
 ang_vel = tf(b(2,:), a);
 
 % save these for later export/ use in simulink
 b1 = b(1,:);
 b2 = b(2,:);
 
-
-% Sameer the undergrad was usng this code to understand the time response
-% a transfer function which originated in the frequency domain to an input 
-% step function 
-syms s
-[num,den] = tfdata(v_lat);
-G_sym     = (poly2sym(num,s)/poly2sym(den,s));
-Y_lap_sym = G_sym/s; % U(s) = 1/s for the unit step
-y_time_sym = ilaplace(Y_lap_sym)
-step(v_lat);
-
-% Sameer also messed around with saving and loading waypoints for a path
-waypoints_x = [0, 1, 2, 3, 4];
-waypoints_y = [0, 1, 3, 4, 5];
-
-save('gatorwaypoints.mat', 'waypoints_x', 'waypoints_y');
-load('gatorwaypoints.mat');
+% run the simulation
+sim("simulation.slx")
